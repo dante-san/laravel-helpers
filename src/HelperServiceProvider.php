e09db2e84a573d2553cb4361d2a567d4f86d92d2
Laxmidhar Maharana <papu.team7@gmail.com>
2025-10-16 21:44:40 +0530

    Fix: Correct namespace and service provider configuration

--- src/HelperServiceProvider.php
+++ src/HelperServiceProvider.php
@@ -1,43 +1,35 @@
 <?php
+// File: src/HelperServiceProvider.php
 
 namespace Laxmidhar\LaravelHelpers;
 
 use Illuminate\Support\ServiceProvider;
+use Laxmidhar\LaravelHelpers\Support\HelperService;
 
 class HelperServiceProvider extends ServiceProvider
 {
     public function register()
     {
-        // Merge config
-        // $this->mergeConfigFrom(
-        //     __DIR__ . '/../config/helpers.php',
-        //     'helpers'
-        // );
-
-        // Register the service
+        // Register the service in the container
         $this->app->singleton('laravel-helpers', function ($app) {
-            return new Support\HelperService();
+            return new HelperService();
         });
+
+        // Also register under a more accessible alias
+        $this->app->alias('laravel-helpers', HelperService::class);
     }
 
     public function boot()
     {
-        // Publish config
-        // if ($this->app->runningInConsole()) {
-        //     $this->publishes([
-        //         __DIR__ . '/../config/helpers.php' => config_path('helpers.php'),
-        //     ], 'helpers-config');
-        // }
-
-        // // Register custom facade alias if configured
-        // $customAlias = config('helpers.facade_alias');
-        // if ($customAlias && $customAlias !== 'Helper') {
-        //     $this->app->alias('laravel-helpers', $customAlias);
-        // }
+        // Load helper functions file if it exists
+        $helperFile = __DIR__ . '/../helpers.php';
+        if (file_exists($helperFile)) {
+            require_once $helperFile;
+        }
     }
 
     public function provides()
     {
-        // return ['laravel-helpers'];
+        return ['laravel-helpers', HelperService::class];
     }
 }
